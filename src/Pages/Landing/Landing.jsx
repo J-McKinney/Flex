8c47f0f73ae2e1ea9b1b40bfdaa9e6b2ed3a0d44
Jesse <jessemckinney513@gmail.com>
2020-08-27 12:17:25 -0400

    yay

--- src/Pages/Landing/Landing.jsx
+++ src/Pages/Landing/Landing.jsx
@@ -1,21 +1,53 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+// import Container from "react-bootstrap/Container";
+// import Row from "react-bootstrap/Row";
+// import Col from "react-bootstrap/Col";
 import Style from "./Landing.module.css";
 
 function Landing() {
   return (
     <>
-      <div className={Style.wrapper}>
-        <Container className={Style.menuContainer}>
-          <div className={Style.perspectiveText}>
-            <div className={Style.perspectiveLine}>
-              <p className={Style.options}></p>
-              <p className={Style.options}>About Me</p>
-            </div>
+      <div className={Style.perspectiveWrapper}>
+        {/**/}
+        <div className={Style.perspectiveText}>
+          <div className={Style.perspectiveLine}>
+            <p className={Style.options}></p>
+            <p className={Style.options}>About Me</p>
           </div>
-        </Container>
+          {/**/}
+          <div className={Style.perspectiveLine}>
+            <p className={Style.options}>About Me</p>
+            <p className={Style.options}></p>
+          </div>
+        </div>
+        {/**/}
+        <br />
+        {/**/}
+        <div className={Style.perspectiveText}>
+          <div className={Style.perspectiveLine}>
+            <p className={Style.options}></p>
+            <p className={Style.options}>My Work</p>
+          </div>
+          {/**/}
+          <div className={Style.perspectiveLine}>
+            <p className={Style.options}>My Work</p>
+            <p className={Style.options}></p>
+          </div>
+        </div>
+        {/**/}
+        <br />
+        {/**/}
+        <div className={Style.perspectiveText}>
+          <div className={Style.perspectiveLine}>
+            <p className={Style.options}></p>
+            <p className={Style.options}>Contact Me</p>
+          </div>
+          {/**/}
+          <div className={Style.perspectiveLine}>
+            <p className={Style.options}>Contact Me</p>
+            <p className={Style.options}></p>
+          </div>
+        </div>
       </div>
     </>
   );
