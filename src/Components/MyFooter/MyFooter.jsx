9632c17aa5aed59c80f290050441b723634bb721
Jesse <jessemckinney513@gmail.com>
2020-09-06 17:33:55 -0400

    yay

--- src/Components/MyFooter/MyFooter.jsx
+++ src/Components/MyFooter/MyFooter.jsx
@@ -17,66 +17,48 @@ class MyFooter extends Component {
           <hr className={FooterStyle.hr} />
           <Row>
             <Col className={FooterStyle.Col}>
-              <ul className={FooterStyle.ul}>
-                <li className={FooterStyle.li}>
-                  <h5 className={FooterStyle.h5}>
-                    <a
-                      className={FooterStyle.a}
-                      href="https://mail.google.com/mail/?view=cm&fs=1&to=JesseMcKinney513&#64gmail.com&su=ContactingYouOnPortfolioEmail"
-                    >
-                      <code>@</code>Gmail
-                    </a>
-                  </h5>
-                </li>
-                <li className={FooterStyle.li}>
-                  <h5 className={FooterStyle.h5}>
-                    <a
-                      className={FooterStyle.a}
-                      href="https://www.linkedin.com/in/jesse-mckinney-4a3b10185?trk=people-guest_profile-result-card_result-card_full-click"
-                    >
-                      LinkedIn
-                    </a>
-                  </h5>
-                </li>
-              </ul>
+              <a
+                className={FooterStyle.a}
+                href="https://mail.google.com/mail/?view=cm&fs=1&to=JesseMcKinney513&#64gmail.com&su=ContactingYouOnPortfolioEmail"
+              >
+                <i id={FooterStyle.envelope} className="far fa-envelope" />
+              </a>
+            </Col>
+          </Row>
+          <br />
+          <Row className={FooterStyle.iconRow}>
+            <Col className={FooterStyle.Col}>
+              <a
+                className={FooterStyle.a}
+                href="https://www.linkedin.com/in/jesse-mckinney-4a3b10185?trk=people-guest_profile-result-card_result-card_full-click"
+              >
+                <i id={FooterStyle.linkedin} className="fab fa-linkedin" />
+              </a>
             </Col>
             <Col className={FooterStyle.Col}>
-              <ul className={FooterStyle.ul}>
-                <li className={FooterStyle.li}>
-                  <h5 className={FooterStyle.h5}>
-                    <a
-                      className={FooterStyle.a}
-                      href="https://github.com/J-McKinney"
-                    >
-                      GitHub
-                    </a>
-                  </h5>
-                </li>
-                <li className={FooterStyle.li}>
-                  <h5 className={FooterStyle.h5}>
-                    <a
-                      className={FooterStyle.a}
-                      href="https://www.pinterest.com/jesse6446/"
-                    >
-                      Pinterest
-                    </a>
-                  </h5>
-                </li>
-                <li className={FooterStyle.li}>
-                  <h5 className={FooterStyle.h5}>
-                    <a
-                      className={FooterStyle.a}
-                      href="https://instagram.com/jessman51386?igshid=sfn5fawa2ea2"
-                    >
-                      Instagram
-                    </a>
-                  </h5>
-                </li>
-              </ul>
+              <a className={FooterStyle.a} href="https://github.com/J-McKinney">
+                <i id={FooterStyle.github} className="fab fa-github" />
+              </a>
+            </Col>
+            <Col className={FooterStyle.Col}>
+              <a
+                className={FooterStyle.a}
+                href="https://www.pinterest.com/jesse6446/"
+              >
+                <i id={FooterStyle.pinterest} className="fab fa-pinterest" />
+              </a>
+            </Col>
+            <Col className={FooterStyle.Col}>
+              <a
+                className={FooterStyle.a}
+                href="https://instagram.com/jessman51386?igshid=sfn5fawa2ea2"
+              >
+                <i id={FooterStyle.instagram} className="fab fa-instagram" />
+              </a>
             </Col>
           </Row>
         </Container>
-        <div className={FooterStyle.bottomName}>Jesse MᶜKinney</div>
+        <div className={FooterStyle.bottomName}>Jesse McKinney</div>
       </>
     );
   }
