9ef4005cb83b5f67dcaf3132e61ab04878592518
Jesse <jessemckinney513@gmail.com>
2020-09-08 13:50:09 -0400

    yay

--- src/Components/BarCharts/BarCharts.jsx
+++ src/Components/BarCharts/BarCharts.jsx
@@ -1,19 +1,48 @@
 import React, { Component } from "react";
 import { HorizontalBar } from "react-chartjs-2";
+import Fade from "react-reveal/Fade";
 import Style from "./BarCharts.module.css";
 
 class BarCharts extends Component {
   componentDidMount() {
-    console.log("componentDidMount: ", HorizontalBar);
+    console.log("componentDidMount: ");
   }
   componentDidUpdate() {
-    console.log("componentDidUpdate: ", HorizontalBar);
+    console.log("componentDidUpdate: ");
   }
 
   render() {
+    const data = {
+      labels: [
+        "HTML",
+        "CSS",
+        "BootStrap",
+        "JavaScript",
+        "React",
+        "Angular",
+        "Node.js",
+        "Mongoose",
+        "MySQL",
+      ],
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgba(255,99,132,0.2)",
+          borderColor: "rgba(255,99,132,1)",
+          borderWidth: 1,
+          hoverBackgroundColor: "rgba(255,99,132,0.4)",
+          hoverBorderColor: "rgba(255,99,132,1)",
+          data: [65, 59, 80, 91, 56, 55, 40, 62, 22],
+        },
+      ],
+    };
     return (
       <>
-        <div className={Style.wrapper}></div>
+        <div className={Style.wrapper}>
+          <Fade right duration={1000} delay={500}>
+            <HorizontalBar data={data} />
+          </Fade>
+        </div>
       </>
     );
   }
