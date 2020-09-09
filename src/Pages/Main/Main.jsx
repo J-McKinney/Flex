fe348a2de8230a2c4ba2f70bf4135856f85be0af
Jesse <jessemckinney513@gmail.com>
2020-09-09 14:11:08 -0400

    yay

--- src/Pages/Main/Main.jsx
+++ src/Pages/Main/Main.jsx
@@ -7,7 +7,6 @@ import { HorizontalBar } from "react-chartjs-2";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Fade from "react-reveal/Fade";
 import Style from "./Main.module.css";
 
 const chart = {
@@ -48,143 +47,125 @@ class Main extends Component {
       <>
         <div className={Style.siteWrapper}>
           <NavBar />
-          <div className={Style.mainWrapper}>
+          <div className={Style.starWrapper}>
             <Stars />
           </div>
           {/* About Me Area */}
           <div className={Style.aboutMeWrapper}>
             <Container className={Style.aboutMeContainer}>
               <Row className={Style.aboutMeHeader}>
-                <Fade down duration={1000} delay={500}>
-                  <h2 className={Style.aboutHeader}>About</h2>
-                </Fade>
+                <h2 className={Style.aboutHeader}>About</h2>
               </Row>
               <hr className={Style.hr1Main} />
               <Row className={Style.aboutMeRow}>
                 <Col className={Style.aboutMeCol}>
-                  <Fade left big duration={1000} delay={500}>
-                    <i id={Style.aboutMeIcon} className="fas fa-fighter-jet" />
-                    <h3 className={Style.aboutMeh3}>Fast/Clean Code</h3>
-                    <p className={Style.aboutMePTags}>
-                      Fast load times and lag free interaction, my highest
-                      priority.
-                    </p>
-                  </Fade>
+                  <i id={Style.aboutMeIcon} className="fas fa-fighter-jet" />
+                  <h3 className={Style.aboutMeh3}>Fast/Clean Code</h3>
+                  <p className={Style.aboutMePTags}>
+                    Fast load times and lag free interaction, my highest
+                    priority.
+                  </p>
                 </Col>
                 <Col className={Style.aboutMeCol}>
-                  <Fade left duration={1000} delay={500}>
-                    <i id={Style.aboutMeIcon} className="fas fa-mobile-alt" />
-                    <h3 className={Style.aboutMeh3}>Responsive</h3>
-                    <p className={Style.aboutMePTags}>
-                      My layouts will work on any device, big or small.
-                    </p>
-                  </Fade>
+                  <i id={Style.aboutMeIcon} className="fas fa-mobile-alt" />
+                  <h3 className={Style.aboutMeh3}>Responsive</h3>
+                  <p className={Style.aboutMePTags}>
+                    My layouts will work on any device, big or small.
+                  </p>
                 </Col>
                 <Col className={Style.aboutMeCol}>
-                  <Fade right duration={1000} delay={500}>
-                    <i id={Style.aboutMeIcon} className="fas fa-code" />
-                    <h3 className={Style.aboutMeh3}>Intuitive</h3>
-                    <p className={Style.aboutMePTags}>
-                      Strong preference for easy to use, intuitive UX/UI.
-                    </p>
-                  </Fade>
+                  <i id={Style.aboutMeIcon} className="fas fa-code" />
+                  <h3 className={Style.aboutMeh3}>Intuitive</h3>
+                  <p className={Style.aboutMePTags}>
+                    Strong preference for easy to use, intuitive UX/UI.
+                  </p>
                 </Col>
                 <Col className={Style.aboutMeCol}>
-                  <Fade right big duration={1000} delay={500}>
-                    <i id={Style.aboutMeIcon} className="fas fa-meteor" />
-                    <h3 className={Style.aboutMeh3}>Dynamic</h3>
-                    <p className={Style.aboutMePTags}>
-                      Websites don't have to be static, I love making pages come
-                      to life.
-                    </p>
-                  </Fade>
+                  <i id={Style.aboutMeIcon} className="fas fa-meteor" />
+                  <h3 className={Style.aboutMeh3}>Dynamic</h3>
+                  <p className={Style.aboutMePTags}>
+                    Websites don't have to be static, I love making pages come
+                    to life.
+                  </p>
                 </Col>
               </Row>
               {/* Bar Chart Container */}
-              <Container className={Style.barChartContainer}>
+              <div className={Style.barChartContainer}>
                 <Row className={Style.barChartRow}>
                   <div className="col-md-6" id={Style.MyPicCol}>
-                    <Fade left duration={1000} delay={500}>
-                      <img className={Style.barChartMyPic} src={Me} alt="Me" />
-                      {/* Put a P Tag here with a short sweet description */}
-                    </Fade>
+                    <img className={Style.barChartMyPic} src={Me} alt="Me" />
                   </div>
                   <div className="col-md-6" id={Style.barChartCol}>
-                    <Fade right duration={1000} delay={500}>
-                      <HorizontalBar
-                        className={Style.barChart}
-                        data={chart}
-                        height={400}
-                        width={400}
-                        options={{
-                          animation: {
-                            duration: 0,
-                          },
-                          events: ["null"],
-                          responsive: true,
-                          maintainAspectRatio: true,
-                          title: {
-                            display: false,
-                          },
-                          legend: {
-                            display: false,
-                          },
-                          tooltips: {
-                            enabled: false,
-                          },
-                          scales: {
-                            yAxes: [
-                              {
-                                stacked: true,
-                                ticks: {
-                                  suggestedMin: 0,
-                                  suggestedMax: 100,
-                                  fontSize: 17,
-                                  beginAtZero: true,
-                                },
-                                gridLines: {
-                                  display: false,
-                                },
+                    <HorizontalBar
+                      className={Style.barChart}
+                      data={chart}
+                      height={400}
+                      width={400}
+                      options={{
+                        events: ["null"],
+                        responsive: true,
+                        maintainAspectRatio: true,
+                        title: {
+                          display: false,
+                        },
+                        legend: {
+                          display: false,
+                        },
+                        tooltips: {
+                          enabled: false,
+                        },
+                        scales: {
+                          yAxes: [
+                            {
+                              stacked: true,
+                              ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 100,
+                                fontSize: 17,
+                                beginAtZero: true,
                               },
-                            ],
-                            xAxes: [
-                              {
-                                stacked: true,
-                                ticks: {
-                                  suggestedMin: 0,
-                                  suggestedMax: 100,
-                                  fontSize: 17,
-                                  beginAtZero: true,
-                                },
-                                display: 0,
+                              gridLines: {
+                                display: false,
                               },
-                            ],
-                          },
-                        }}
-                      />
-                    </Fade>
+                            },
+                          ],
+                          xAxes: [
+                            {
+                              stacked: true,
+                              ticks: {
+                                suggestedMin: 0,
+                                suggestedMax: 100,
+                                fontSize: 17,
+                                beginAtZero: true,
+                              },
+                              display: 0,
+                            },
+                          ],
+                        },
+                      }}
+                    />
                   </div>
                 </Row>
-              </Container>
+              </div>
               {/* Bar Chart Container */}
             </Container>
           </div>
           {/* About Me Area */}
-          {/* Curved Div */}
-          <svg
-            className={Style.svg}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 1440 320"
-          >
-            <path
-              fill="#282c34"
-              fillOpacity="1"
-              d="M0,256L48,261.3C96,267,192,277,288,256C384,235,480,181,576,181.3C672,181,768,235,864,256C960,277,1056,267,1152,266.7C1248,267,1344,277,1392,282.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-            ></path>
-          </svg>
-          {/* Curved Div */}
-          <Footer />
         </div>
+        {/* Curved Div */}
+        <svg
+          className={Style.svg}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#282c34"
+            fillOpacity="1"
+            d="M0,256L48,261.3C96,267,192,277,288,256C384,235,480,181,576,181.3C672,181,768,235,864,256C960,277,1056,267,1152,266.7C1248,267,1344,277,1392,282.7L1440,288L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
+          ></path>
+        </svg>
+        {/* Curved Div */}
+        <Footer />
       </>
     );
   }
